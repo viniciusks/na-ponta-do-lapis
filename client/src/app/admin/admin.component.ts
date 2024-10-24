94530d759ee3c25ccd191f5137b4f5837ab54137
Vinicius Kremer Santos <vinicius.kremer@gmail.com>
2024-10-24 00:51:29 -0300

    [#1] Finalizando configuração do Prettier.

--- client/src/app/admin/admin.component.ts
+++ client/src/app/admin/admin.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core'
-import { RouterOutlet } from '@angular/router'
+import { Component, OnInit } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-admin',

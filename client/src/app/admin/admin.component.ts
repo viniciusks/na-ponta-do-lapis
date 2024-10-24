a53f74119400d8156c2491e35e513bb6e7501864
Vinicius Kremer Santos <vinicius.kremer@gmail.com>
2024-10-24 01:34:28 -0300

    [#1] Ajustando layout do painel administrativo.

--- client/src/app/admin/admin.component.ts
+++ client/src/app/admin/admin.component.ts
@@ -1,15 +1,48 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MenuModule } from 'primeng/menu';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-admin',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MenuModule],
   templateUrl: './admin.component.html',
   styleUrl: './admin.component.css',
 })
 export class AdminComponent implements OnInit {
+  items: MenuItem[] | undefined;
+
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.items = [
+      {
+        label: 'Documents',
+        items: [
+          {
+            label: 'New',
+            icon: 'pi pi-plus',
+          },
+          {
+            label: 'Search',
+            icon: 'pi pi-search',
+          },
+        ],
+      },
+      {
+        label: 'Profile',
+        items: [
+          {
+            label: 'Settings',
+            icon: 'pi pi-cog',
+          },
+          {
+            label: 'Logout',
+            icon: 'pi pi-sign-out',
+          },
+        ],
+      },
+    ];
+  }
 }

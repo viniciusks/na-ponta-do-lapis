8735b83bdc3760e65a9ede1f486c63d0693be3c7
Vinicius Kremer Santos <vinicius.kremer@gmail.com>
2024-10-24 00:39:40 -0300

    [#1] Retirando primeng, adicionando componente admin, adicionando fonte Poppins e instalando prettier.

---
+++ client/src/app/admin/admin.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core'
+import { RouterOutlet } from '@angular/router'
+
+@Component({
+  selector: 'app-admin',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './admin.component.html',
+  styleUrl: './admin.component.css',
+})
+export class AdminComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router'
 import { UsersAdminComponent } from './admin/users-admin/users-admin.component'
 import { HomeAdminComponent } from './admin/home-admin/home-admin.component'
+import { AdminComponent } from './admin/admin.component'
 
 export const routes: Routes = [
   {
     path: 'admin',
+    component: AdminComponent,
     children: [
       {
         path: '',

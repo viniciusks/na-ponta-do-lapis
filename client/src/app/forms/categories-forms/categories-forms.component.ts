fea4e68f591ba943b4d7a1027651cbe80403a697
Vinicius Kremer Santos <vinicius.kremer@gmail.com>
2024-12-19 09:42:23 -0300

    [#10] Implement category form with input fields and submission handling

--- client/src/app/forms/categories-forms/categories-forms.component.ts
+++ client/src/app/forms/categories-forms/categories-forms.component.ts
@@ -1,23 +1,45 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+import { ToastModule } from 'primeng/toast';
+import { Category } from '../../models/category';
+import { MessageService } from 'primeng/api';
+import { FormsModule } from '@angular/forms';
+import { InputTextModule } from 'primeng/inputtext';
 
 @Component({
   selector: 'app-categories-forms',
   standalone: true,
-  imports: [],
+  imports: [
+    ButtonModule,
+    RouterLink,
+    ToastModule,
+    FormsModule,
+    InputTextModule,
+  ],
   templateUrl: './categories-forms.component.html',
   styleUrl: './categories-forms.component.css',
+  providers: [MessageService],
 })
 export class CategoriesFormsComponent implements OnInit {
   action: string;
+  category: Category;
 
   constructor(private _activatedRoute: ActivatedRoute) {
     this.action = '';
+    this.category = {
+      name: '',
+      description: '',
+    };
   }
 
   ngOnInit(): void {
     this._activatedRoute.params.subscribe((params) => {
       this.action = params['action'];
     });
+  }
+
+  onSubmit(): void {
+    console.log(this.category);
   }
 }
